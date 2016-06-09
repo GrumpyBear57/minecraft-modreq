f349bae9424d047de3a696d85fe298d7c4527c3a
GrumpyBear57 <gb57mail@yahoo.ca>
2016-06-08 20:40:05 -0600

    config files and first command
    NOT WORKING!
    Doesn't compile...

--- src/com/grumpybear/modreq/main.java
+++ src/com/grumpybear/modreq/main.java
@@ -1,32 +1,88 @@
 package com.grumpybear.modreq;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.zaxxer.hikari.HikariDataSource;
 
 public class main extends JavaPlugin implements Listener {
+	// version
+	String version = "v1.0.0";
 	
+	// chat output vars
+	String prefix = ChatColor.RED + "[" + ChatColor.GREEN + "Mod Request" + ChatColor.RED + "] " + ChatColor.GOLD;
+	String noPerm = ChatColor.RED + "You don't have permission to perform that command!";
+	
+	// database stuffs  
 	private HikariDataSource hikari;
+	PreparedStatement p = null;
+	Connection connection = null;
+	
+	// config file stuffs
+	private File configf, databasef;
+	private FileConfiguration config, database;
 	
 	@Override
 	public void onEnable() {
+		getLogger().info("Connecting to database...");
 		connectDB();
+		getLogger().info("Database connected.");
+		createTable();
+		createConfig();
+		getLogger().info("Successfully loaded modreq " + version);
 	}
 	
 	@Override
 	public void onDisable() {
 	}
 	
+	public FileConfiguration getDatabaseConfig() {
+		return this.database;
+	}
+	
+	private void createConfig() {
+		configf = new File(getDataFolder(), "config.yml");
+		databasef = new File(getDataFolder(), "database.yml");
+		
+		if (!configf.exists()) {
+			getLogger().info("config.yml doesn't exist... creating...");
+			configf.getParentFile().mkdirs();
+			saveResource("config.yml");
+		}
+		if (!databasef.exists()) {
+			getLogger().info("database.yml doesn't exist... creating...");
+			databasef.getParentFile().mkdirs();
+			saveResource("database.yml");
+		}
+		
+		config = new YamlConfiguration();
+		database = new YamlConfiguration();
+		try {
+			config.load(configf);
+			database.load(databasef);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public void connectDB() {
-		String address = getConfig().getString("Database.Address");
-		String name = getConfig().getString("Database.Name");
-		String username = getConfig().getString("Database.Username");
-		String password = getConfig().getString("Database.Password");
+		String address = plugin.getDatabaseConfig().getString("Database.Address");
+		String name = plugin.getDatabaseConfig().getString("Database.Name");
+		String username = plugin.getDatabaseConfig().getString("Database.Username");
+		String password = plugin.getDatabaseConfig().getString("Database.Password");
 		
 		hikari = new HikariDataSource();
 		hikari.setMaximumPoolSize(8);
@@ -39,7 +95,6 @@ public class main extends JavaPlugin implements Listener {
 	}
 
 	public void createTable() {
-		Connection connection = null;
 		String createTable = "CREATE TABLE IF NOT EXISTS requests " +
 				  "(id INT NOT NULL AUTO_INCREMENT, " +
 				  "user VARCHAR(32) NOT NULL, "+
@@ -57,8 +112,6 @@ public class main extends JavaPlugin implements Listener {
 				  "resolution VARCHAR(100) NULL, " +
 				  "escalated TINYINT NULL, " +
 				  "PRIMARY KEY (id)) ";
-		
-		PreparedStatement p = null;
 		
 		try {
 			connection = hikari.getConnection();
@@ -86,6 +139,55 @@ public class main extends JavaPlugin implements Listener {
 		}
 	}
 	
+	public class commandModreq implements CommandExecutor {
+
+		@Override
+		public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+			if (args.length == 0) {
+				sender.sendMessage(prefix + ChatColor.GOLD + "This server is running " + ChatColor.GREEN + "Mod Request " + version + ChatColor.GOLD + " by GrumpyBear57!");
+				sender.sendMessage(ChatColor.GOLD + "To submit a request, do /modreq <request>");
+				sender.sendMessage("Licensed under Apache v2.0, Copyright 2016 GrumpyBear57");
+			} else if (!(sender instanceof Player)) { 
+				sender.sendMessage("You must be a player to perform this command!"); //TODO find out if this works
+			} else { 
+				Player player = (Player) sender;
+				int ticketNumber = 42; //this is temporary until we get the request database going.
+				
+				if(player.hasPermission("modreq.newReq")) {
+					String request = null;
+					for (int i = 0; i < args.length; i++) {
+						if (i != args.length-1) {
+							request += args[i] + " ";
+						} else {
+							request += args[i];
+						}
+					}
+					String query = ""; //TODO actual query
+					
+					try {
+						connection = hikari.getConnection();
+						p = connection.prepareStatement(query);
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					
+					sender.sendMessage(prefix + "Your request has been added to the queue. Your ticket number is " + ticketNumber + ".");
+					sender.sendMessage(ChatColor.GOLD + "Your request: '" + request + "'");
+					
+					Player[] playersOnline = Bukkit.getServer().getOnlinePlayers().toArray(new Player[Bukkit.getServer().getOnlinePlayers().size()]);
+					for (int i = 0; i < playersOnline.length; i++) {
+						Player player1 = (Player) playersOnline[i];
+						if (player1.hasPermission("modreq.veiwQueue")) {
+							player1.sendMessage(prefix + ChatColor.GREEN + player.getDisplayName() + ChatColor.GOLD + " has submitted a new request!");
+						}
+					}
+				} else {
+					sender.sendMessage(noPerm);
+				}
+			}
+			return false;
+		}
+		
+	}
+	
 }
-
-
